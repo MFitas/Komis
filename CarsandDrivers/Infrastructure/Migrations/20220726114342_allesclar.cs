58fea3fb90927dde69ce486b37034ea8656134a2
Mateusz Fitas <mateuszfitas1990@gmail.com>
2022-07-26 14:46:46 +0200

    added missing migrations

---
+++ CarsandDrivers/Infrastructure/Migrations/20220726114342_allesclar.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarsAndDrivers.Migrations
+{
+    public partial class allesclar : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CarModels_CarBrands_BrandId",
+                table: "CarModels");
+
+            migrationBuilder.DropColumn(
+                name: "Brand",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "Model",
+                table: "Cars");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BrandId",
+                table: "Cars",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ModelId",
+                table: "Cars",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BrandId",
+                table: "CarModels",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_ModelId",
+                table: "Cars",
+                column: "ModelId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CarModels_CarBrands_BrandId",
+                table: "CarModels",
+                column: "BrandId",
+                principalTable: "CarBrands",
+                principalColumn: "BrandId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cars_CarModels_ModelId",
+                table: "Cars",
+                column: "ModelId",
+                principalTable: "CarModels",
+                principalColumn: "ModelId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CarModels_CarBrands_BrandId",
+                table: "CarModels");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cars_CarModels_ModelId",
+                table: "Cars");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cars_ModelId",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "BrandId",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "ModelId",
+                table: "Cars");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Brand",
+                table: "Cars",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Model",
+                table: "Cars",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BrandId",
+                table: "CarModels",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CarModels_CarBrands_BrandId",
+                table: "CarModels",
+                column: "BrandId",
+                principalTable: "CarBrands",
+                principalColumn: "BrandId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
