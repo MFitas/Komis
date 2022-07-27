4c5de2592ddd6bfca8c8081c678ad51db24a254d
Mateusz Fitas <mateuszfitas1990@gmail.com>
2022-07-27 15:50:46 +0200

    missing migrations

---
+++ CarsandDrivers/Infrastructure/Migrations/20220727093015_testseed.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarsAndDrivers.Migrations
+{
+    public partial class testseed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "CarBrands",
+                columns: new[] { "BrandId", "BrandName" },
+                values: new object[,]
+                {
+                    { 1, "Volvo" },
+                    { 4, "Fiat" },
+                    { 3, "Volvo" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "CarBrands",
+                keyColumn: "BrandId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "CarBrands",
+                keyColumn: "BrandId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "CarBrands",
+                keyColumn: "BrandId",
+                keyValue: 4);
+        }
+    }
+}
